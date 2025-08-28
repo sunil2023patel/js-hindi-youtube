0a1b76c53a3cf1eb99451ed359c159d2a95cf56d
sunil2023patel <sameerpatel810910@gmail.com>
2025-08-28 10:16:48 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,3 +6,7 @@ let accountstate;
 // accountId=93324
 console.log(accountId);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+/*
+Prefer not to use var
+because of issue in block scope ans functional scope
+*/
