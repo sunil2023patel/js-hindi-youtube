68c0f5718a1f74aa00dfed7815ecd813eed7417d
sunil2023patel <sameerpatel810910@gmail.com>
2025-08-30 03:49:45 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -8,5 +8,5 @@ console.log(accountId);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
 /*
 Prefer not to use var
-because of issue in block scope ans functional scope
+because of issue in block scope and functional scope
 */ 
