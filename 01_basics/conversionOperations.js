86c2e3f06a52d0c133375bc6a3f0bcfbee806b33
sunil2023patel <sameerpatel810910@gmail.com>
2026-06-29 23:21:32 +0000

    final update

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -1,3 +1,60 @@
 
 
 "use strict"
+let score=null
+let scoreInNumber=Number(score)
+
+// console.log(score)
+// console.log(typeof scoreInNumber)
+// console.log(scoreInNumber)
+// string to Number
+// "33"=> 33
+// "33abc"=>NaN
+// true=>1 false=>0
+// ""=> 0 null =>0
+// string to boolean
+// ""=> false
+// sunil=> true
+
+let isloggedIn=1
+let booleanIsloggedIn=Boolean(isloggedIn)
+// console.log(booleanIsloggedIn)
+// ************************* Operations ***********************************
+let value=3
+let negValue=-value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) power
+// console.log(2/3)
+// console.log(2%3)
+let st1="vashundhara "
+let st2="and shikha"
+
+console.log(st1+st2)
+console.log(2*2+"1"+2/2 + 4) //4114
+console.log(1+"2") // 12
+console.log("1"+2+4) //124
+console.log(2+4+"4") //64
+console.log(+true)
+
+let num1 ,num2,num3
+num1=num2=num3=2+2
+// console.log(num3)
+let gameCounter=199
+let y=++gameCounter
+let x=gameCounter++
+console.log(gameCounter)//200
+console.log(x)//200
+console.log(y)// 200
+console.log(gameCounter)//201
+
+
+
+
+
+
+
+
+
