7fc11030339e9ddbeb71c5901ee639f0f65fa868
sunil2023patel <sameerpatel810910@gmail.com>
2026-07-06 08:02:50 +0000

    math and num

---
+++ 01_basics/numbers.js
@@ -0,0 +1,25 @@
+const score=400
+const balance=new Number(100)
+// console.log(balance)
+const str1=new String("hello")
+// console.log(str1)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+// const othernumber=123.9898
+// console.log(othernumber.toPrecision(2))
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// +++++++++++Maths++++++++++
+// console.log(Math)
+// console.log(Math.abs(-8))
+// console.log(Math.round(9.1))
+// console.log(Math.ceil(8.22))
+// console.log(Math.floor(7.9))
+// console.log((Math.random()*10)+1) // values between 0 to 1 
+// console.log((Math.floor(random()*10))+1)
+const min=10;
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
