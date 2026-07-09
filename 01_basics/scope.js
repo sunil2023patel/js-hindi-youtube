a9f2c24ce40fa0dc6826d7ca50d324ad2d99acbb
sunil2023patel <sameerpatel810910@gmail.com>
2026-07-09 00:45:36 +0000

    more on scopes

--- 01_basics/scope.js
+++ 01_basics/scope.js
@@ -6,8 +6,37 @@ if(true){
     var c=9
 
     console.log("INNER:",a)
-    
+
 }
 console.log("OUTER:",a)
 // console.log(b)
 console.log(c)
+function one(){
+    const username="hitesh"
+    function two(){
+        const website="youtube"
+        // console.log(username)
+    }
+    // console.log(website)
+    // two()
+}
+one()
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website="youtube"
+    //     console.log(username+website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+// +++++++++++++interesting++++++++++++++
+console.log(addone(8))
+function addone(num){
+    return num+1
+}
+// console.log(addone(8))
+console.log(addtwo(8))
+const addtwo=function(num){
+    return num+2
+}
