9fea3615b32f51f6629cb95701bb5b3f0b643aae
sunil2023patel <sameerpatel810910@gmail.com>
2026-07-09 00:16:39 +0000

    more on function

--- 01_basics/functions.js
+++ 01_basics/functions.js
@@ -28,3 +28,26 @@ function loginusermessage(username="sam"){
 }
 // let message=loginusermessage("shikha")
 console.log(loginusermessage())
+function calculatecardpricea(val1,val2,...num1){// here ... is a rest operator
+    return num1
+}
+console.log(calculatecardpricea(7,9,8,6))
+const user={
+    username:"hitesh",
+    prices:999
+
+}
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleobject(user)
+handleobject({
+    username:"sam",
+    price:90
+})
+const mynewarr=[90,80,70,78]
+function resecondval(getarr){
+    return getarr[0]
+}
+console.log(resecondval(mynewarr))
